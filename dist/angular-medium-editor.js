d2769b496dcabab07bc8c57fa5669bfbf3ca87b7
Ava Collins <avacollins@comcast.net>
2015-04-29 12:11:17 -0700

    Add branch logic to show placeholder when model has been programmatically changed. *NOTE* must add .medium-editor-placeholder {min-height:30px;} to your CSS

--- dist/angular-medium-editor.js
+++ dist/angular-medium-editor.js
@@ -73,9 +73,17 @@ angular.module('angular-medium-editor', [])
           }
 
           iElement.html(ctrl.$isEmpty(ctrl.$viewValue) ? '' : ctrl.$viewValue);
-          
+
           // hide placeholder when view is not empty
-          if(!ctrl.$isEmpty(ctrl.$viewValue)) angular.element(iElement).removeClass('medium-editor-placeholder'); 
+          if(!ctrl.$isEmpty(ctrl.$viewValue)) angular.element(iElement).removeClass('medium-editor-placeholder');
+
+          // show placeholder when model has been programmatically changed
+          if(ctrl.$isEmpty(ctrl.$viewValue)) {
+            scope.$evalAsync(function(){
+              opts.placeholder = placeholder;
+              var editor = new MediumEditor(iElement, opts);
+            });
+          }
         };
 
       }
